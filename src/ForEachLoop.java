69b93a11953bf36e93966358a104610636d0b433
karaolinabrylowska <kar.bronk@gmail.com>
2022-09-20 20:03:17 +0200

    alter for each loop

---
+++ src/ForEachLoop.java
@@ -0,0 +1,9 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        String[] cars=new String[] {"Audi", "BMW", "Fiat", "Mercedes"};
+        for(String car:cars){
+            System.out.println(car);
+        }
+
+    }
+}
